c8f9a57bf3ce7fece4f193526cd79d8bfc915607
arikarim <arikarim146@gmail.com>
2021-03-05 23:28:54 +0300

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    validates :title, :body, presence: true
+  validates :title, :body, presence: true
 
-    has_many :comments
-    belongs_to :user
+  has_many :comments
+  belongs_to :user
 end
